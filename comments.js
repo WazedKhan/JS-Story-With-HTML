a41e9ea07f9c784ba9087b8797436ff6c00b8c91
WazedKhan <wazedkhan119399@gmail.com>
2023-06-22 20:05:39 +0600

    story of JS with HTML

---
+++ comments.js
@@ -0,0 +1,5 @@
+var number = 5; // in line comment
+/*
+multi line comment
+*/
+number = 9;
---
+++ data_type.js
@@ -0,0 +1,22 @@
+/*
+Data types
+undefined, null, boolean, string, symbol, number and object
+*/
+
+// var is global, life is within the whole program
+var myName = "Finn";
+
+myName = 8;
+var a;
+console.log(a);
+a = 7;
+b = a;
+console.log(b);
+
+const price1 = 10;
+const price2 = 10;
+
+let x = price1 + price2;
+
+console.log(x);
+
