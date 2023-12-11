4e6342e2d04803e88fa160a46f8c769fe08b43b3
WazedKhan <wazedkhan119399@gmail.com>
2023-12-11 09:17:43 +0600

    new starting

---
+++ base/hello.js
@@ -0,0 +1,12 @@
+const myHeading = document.querySelector("h1")
+myHeading.textContent = "Hello World!"
+
+
+let iceCream = "chocolates";
+
+if (iceCream == "chocolate") {
+    alert("Yay, I love chocolate ice cream!");
+}
+else {
+    alert("Awwww, but chocolate is my favorite…"+ iceCream);
+}
